bde332bf99340d735d62c6f1c5cefaac621a081f
hsnks100 <hsnks100@gmail.com>
2016-09-27 09:38:03 +0900

    Update githook.php

--- githook.php
+++ githook.php
@@ -1,14 +1,17 @@
 <?php
 
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /home/gsoo/public_html && git reset --hard HEAD && git pull' );
+}
 //$output = shell_exec( 'cd /home/gsoo/public_html && git reset --hard HEAD && git pull origin master' );
 //echo $output;
 //echo "end";
 //
-shell_exec('cd /home/gsoo/public_html');
+// shell_exec('cd /home/gsoo/public_html');
 //exec("git add -A");
 //exec('git commit -a -m "hook"');
-exec("pwd && git pull" , $output, $error);
-print_r($output);
-print_r($error);
+// exec("pwd && git pull" , $output, $error);
+// print_r($output);
+// print_r($error);
 
 ?>
