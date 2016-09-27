a1f2280589a9d8484a966098570c0cb9b62846e9
home-ubuntu <hsnks100@gmail.com>
2016-09-27 09:12:26 +0900

    hook

---
+++ githook.php
@@ -0,0 +1,14 @@
+<?php
+
+//$output = shell_exec( 'cd /home/gsoo/public_html && git reset --hard HEAD && git pull origin master' );
+//echo $output;
+//echo "end";
+//
+shell_exec('cd /home/gsoo/public_html');
+exec("git add -A");
+exec('git commit -a -m "hook');
+exec("git pull origin master" , $output, $error);
+print_r($output);
+print_r($error);
+
+?>
